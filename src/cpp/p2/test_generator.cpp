96a2687e95394cd490283d20eb2220714ecaf50b
Fernando Gasperi Jabalera <fgasperijabalera@gmail.com>
2014-10-02 09:42:31 -0300

    P2: experimentación terminada

--- src/cpp/p2/test_generator.cpp
+++ src/cpp/p2/test_generator.cpp
@@ -6,11 +6,11 @@
 
 int main() {
   // test configuration
-  const int instances = 100;
+  const int instances = 200;
   const int start = 4;
   const bool vary_n = false;
-  const int constant_horses = 4;
-  const int constant_n = 1000;
+  const int constant_horses = 5;
+  const int constant_n = 100;
   srand(time(NULL));
 
   std::ofstream ofs;
@@ -24,16 +24,24 @@ int main() {
 
   ofs << instances << std::endl;
 
-  for (int i = start; i < instances; i++) {
+  for (int i = 2; i < instances; i++) {
     if (vary_n) {
-      ofs << i << ' ' << constant_horses << std::endl;
-      for (int j = 0; j < constant_horses; j++) {
-        ofs << rand() % i << ' ' << rand() % i << std::endl;
+      for (int l = 0; l < 3; l++) {
+        for (int m = 0; m < 10; m++) {
+          ofs << i << ' ' << constant_horses+5*l << std::endl;
+          for (int j = 0; j < constant_horses+5*l; j++) {
+            ofs << rand() % i << ' ' << rand() % i << std::endl;
+          }
+        }
       }
     } else {
-      ofs << constant_n << ' ' << i << std::endl;
-      for (int j = 0; j < i; j++) {
-        ofs << rand() % constant_n << ' ' << rand() % constant_n << std::endl;
+      for (int l = 0; l < 3; l++) {
+        for (int m = 0; m < 10; m++) {
+          ofs << 40+l*30 << ' ' << i << std::endl;
+          for (int j = 0; j < i; j++) {
+            ofs << rand() % 40+l*30 << ' ' << rand() % 40+l*30 << std::endl;
+          }
+        }
       }
     }
   }
--- src/cpp/p2/tests.cpp
+++ src/cpp/p2/tests.cpp
@@ -12,7 +12,7 @@
 
 int main() {
  // configuration
- const bool test_n = true;
+ const bool test_n = false;
 
  std::string in_file;
  std::string out_file;
@@ -33,29 +33,42 @@ int main() {
 
  ifs >> instances;
 
- for (int i = 0; i < instances; i++) {
-   std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-
+ for (int i = 4; i < instances; i++) {
+   std::cout << "PROCESSING INSTANCE " << i << endl;
    int n, k;
    ifs >> n >> k;
-   Board board(n, k);
-   int r, c;
-   for (int i = 0; i < k; i++) {
-     ifs >> r >> c;
-     bfs(i, Square(r, c), board);
+   std::cout << "n: " << n << " k: " << k << endl;
+
+   vector<vector<int> > horses (k, vector<int>(2, -1));
+   for (int h = 0; h < k; h++) {
+     ifs >> horses[h][0] >> horses[h][1];
    }
 
-   vector<int> result = board.get_min_sum();
+   long long int minimum = std::chrono::microseconds::max().count();
+   for (int j = 0; j < 2; j++) {
+     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 
-   std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
-   //std::chrono::duration<std::chrono::microseconds> time_span = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+     Board board(n, k);
+     
+     for (int l = 0; l < k; l++) {
+       bfs(l, Square(horses[l][0], horses[l][1]), board);
+     }
+     
+
+     vector<int> result = board.get_min_sum();
+
+     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+     long long int current = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+     if (current < minimum) {
+       minimum = current;
+     }
+   }
 
    if (test_n) {
-     //ofs << n << ' ' << time_span.count() << endl;
-     ofs << n << ' ' << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << endl;
+     ofs << n << ' ' << minimum/n << endl;
    } else {
-     ofs << k << ' ' << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << endl;
-     //ofs << k << ' ' << time_span.count() << endl;
+     ofs << k << ' ' << minimum << endl;
    }
  }
 
